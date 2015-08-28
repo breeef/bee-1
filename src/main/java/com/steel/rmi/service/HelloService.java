d55db39bd0828eed0cbbf6a6a6e2ac3c5b422bb9
gang.qin <gang.qin@dianping.com>
2015-08-28 16:40:47 +0800

    rmi demo

---
+++ src/main/java/com/steel/rmi/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.steel.rmi.service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by gang.qin on 2015/8/27.
+ */
+public interface HelloService extends Remote {
+
+    public String sayHello(String name) throws RemoteException;
+}
